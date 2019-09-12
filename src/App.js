768225e8026e6106fb9031cadd6b1ac1833368c9
hubenschmidt <whubenschmidt@gmail.com>
2019-09-11 21:16:37 -0400

    formatting intro

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import StickyFooter from "../src/components/StickyFooter";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Introduction from "../src/components/Introduction"
@@ -19,7 +18,7 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     flexDirection: "column",
     minHeight: "100vh",
-    background: 'pink'
+    background: 'black'
   },
   main: {
     marginTop: theme.spacing(8),
@@ -31,7 +30,7 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: "white"
   },
   container: {
-    background: "blue"
+    // background: "black"
   },
   grid: {
     border: "1px solid blue"
@@ -73,9 +72,7 @@ function App() {
             <Paper className={classes.paper}>xs=12</Paper>
           </Grid>
           <Grid item xs={12} sm={6} className={classes.grid2}>
-            <Introduction></Introduction>
-    
-        
+            <Introduction />
           </Grid>
           <Grid item xs={12} sm={6} className={classes.grid2}>
             <Paper className={classes.paper}>xs=12 sm=6</Paper>
--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -1,29 +1,55 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
-    root: {
-        overflow: 'hidden'
-
-    },
-    grayLine: {
-        color: 'gray',
-        // overflow: 'hidden',
-        // display: 'inline-block',
-        whiteSpace: 'nowrap'
-    }
-})
+  root: {
+    overflow: "hidden"
+  },
+  blue: {
+    color: "blue"
+  },
+  yellow: {
+    color: "yellow"
+  },
+  grayLine: {
+    color: "gray",
+    // overflow: 'hidden',
+    // display: 'inline-block',
+    whiteSpace: "nowrap"
+  }
+});
 
 // export default Introduction = () => {
 export default function Introduction() {
   const classes = useStyles();
   return (
-  <Box className={classes.root}>
-      <span className={classes.grayLine}>
+    <Box component="div" className={classes.root}>
+      <Box component="span" className={classes.grayLine}>
+        <Typography variant="body1">
           // introduction
           --------------------------------------------------------------------------------------------------------------------------------------------
-      </span>
-  </Box>
-  )
-};
+        </Typography>
+      </Box>
+      <Box component="div">
+        <Box component="span">
+          <Typography variant="body1" className={classes.blue} display="inline">const </Typography>
+          <Typography variant="body1" className={classes.yellow} display="inline">displayWork </Typography>
+        </Box>
+      </Box>
+      // <span class="yellow">displayWork</span> = //{" "}
+      <span className="yellow">() </span>
+      // <span className="blue">=> </span>
+      // {"{"}
+      // <br />
+      //{" "}
+      <div className="string-indent">
+        // <span className="fuschia">return </span>
+        // <span className="orange">'programming and web development'</span>
+        // <br />
+        //{" "}
+      </div>
+    </Box>
+  );
+}
