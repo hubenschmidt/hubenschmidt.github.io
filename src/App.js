64e7a73179c05f675844610564a4b290686d0351
hubenschmidt <whubenschmidt@gmail.com>
2019-09-12 02:50:11 -0400

    adding state and project data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
-import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/styles"
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Introduction from "../src/components/Introduction";
-import Projects from "../src/components/Projects"
+import Projects from "../src/components/Projects";
+import projectData from "../src/projects.json"
 
 //grid examples
 
-const useStyles = makeStyles(theme => ({
+// const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   root: {
     flexGrow: 0,
     display: "flex",
@@ -23,13 +26,12 @@ const useStyles = makeStyles(theme => ({
   },
 
   footer: {
-    padding: theme.spacing(10),
+    // padding: theme.spacing(10),
     marginTop: "auto",
     backgroundColor: "white"
   },
   container: {
     // background: "black"
-    
   },
   grid: {
     border: "1px dotted blue",
@@ -44,12 +46,13 @@ const useStyles = makeStyles(theme => ({
     marginBottom: "2px"
   },
   paper: {
-    padding: theme.spacing(2),
+    // padding: theme.spacing(2),
     textAlign: "center",
-    color: theme.palette.text.secondary,
-    backgroundColor: 'black'
+    // color: theme.palette.text.secondary,
+    backgroundColor: "black"
   }
-}));
+  
+});
 
 function Copyright() {
   return (
@@ -64,50 +67,56 @@ function Copyright() {
   );
 }
 
-function App() {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Container maxWidth="lg" className={classes.container}>
-        <Grid container>
-          <Grid item xs={12} className={classes.grid}>
-            <Paper className={classes.paper}>xs=12</Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} className={classes.grid2}>
-            <Introduction spacing={0}/>
-            <Grid item xs={12} sm={12} className>
-             <Projects 
-             header="// projects--------------------------------------------------------------------------------------------------------------------------------------------" />
+// function App() {
+class App extends Component {
+  state = {
+    projectData
+  }
+  render() {
+    // const classes = useStyles();
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container>
+            <Grid item xs={12} className={classes.grid}>
+              <Paper className={classes.paper}>xs=12</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} className={classes.grid2}>
+              <Introduction spacing={0} />
+              <Grid item xs={12} sm={12} className>
+                <Projects header="// projects--------------------------------------------------------------------------------------------------------------------------------------------" />
+              </Grid>
+            </Grid>
+            <Grid item xs={12} sm={6} className={classes.grid2}>
+              <Paper className={classes.paper}>window xs=12 sm=6</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3} className={classes.grid3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3} className={classes.grid3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3} className={classes.grid3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3} className={classes.grid3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
             </Grid>
           </Grid>
-          <Grid item xs={12} sm={6} className={classes.grid2}>
-            <Paper className={classes.paper}>window xs=12 sm=6</Paper>
-          </Grid>
-          <Grid item xs={6} sm={3} className={classes.grid3}>
-            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-          </Grid>
-          <Grid item xs={6} sm={3} className={classes.grid3}>
-            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-          </Grid>
-          <Grid item xs={6} sm={3} className={classes.grid3}>
-            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-          </Grid>
-          <Grid item xs={6} sm={3} className={classes.grid3}>
-            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-          </Grid>
-        </Grid>
-      </Container>
-      <footer className={classes.footer}>
-        <Container maxWidth="sm">
-          <Typography variant="body1">
-            My sticky footer can be found here.
-          </Typography>
-          <Copyright />
         </Container>
-      </footer>
-    </div>
-  );
+        <footer className={classes.footer}>
+          <Container maxWidth="sm">
+            <Typography variant="body1">
+              My sticky footer can be found here.
+            </Typography>
+            <Copyright />
+          </Container>
+        </footer>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
