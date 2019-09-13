09223f298de5dd41f8bdc81a0c82eab2663edda8
hubenschmidt <whubenschmidt@gmail.com>
2019-09-13 19:40:34 -0400

    state showing on console log on projectView component

--- src/components/ProjectView/index.js
+++ src/components/ProjectView/index.js
@@ -23,20 +23,33 @@ class ProjectView extends Component {
   // const project = this.props.match.params.id
   // console.log(project)
   // const project = props.projectData.find(proj => proj.id == ownProps.match.params.projId)
+  state = {
+    project: {},
+  }
 
   componentDidMount() {
     const projId = this.props.match.params.id;
-    console.log(projId);
-    console.log(API.getProjects(projId))
+    const results = API.getProjects(projId)
+    this.setState( { project: results })
+  }
+
+  componentDidUpdate(){
+    // console.log(this.state.project[0])
+    const data = this.state.project[0]
+    // console.log(data.url)
+    // this.render()
   }
 
   render() {
     const { classes } = this.props;
+    console.log(this.state.project[0])
+
+
     return (
       <Box component="div" className={classes.root}>
         <Typography variant="body1" className={classes.header}>
           <h3>test</h3>
-          {/* <h2>{props.url}</h2> */}
+          {/* <h2>{this.state}</h2> */}
         </Typography>
       </Box>
     );
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,14 +1,10 @@
 import projectData from "../projects.json";
+import axios from "axios";
 
 export default {
-
-
-    getProjects: function(id){
-
-        const result = projectData.filter(proj => proj.id == id);
-
-        console.log(result)
-        }
-       
-    }
-
+  getProjects: function(id) {
+    // return axios.get(projectData.filter(proj => proj.id == id))
+    const result = projectData.filter(proj => proj.id == id);
+    return result;
+  }
+};
