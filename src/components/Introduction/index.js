0f04e9dcfcd3712e230564ff3cc9fab104827e74
hubenschmidt <whubenschmidt@gmail.com>
2019-09-16 00:16:40 -0400

    style

--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -6,7 +6,6 @@ import Typography from "@material-ui/core/Typography";
 const useStyles = makeStyles({
   root: {
     overflow: "hidden"
-    
   },
   blue: {
     color: "#258BFF"
@@ -18,7 +17,7 @@ const useStyles = makeStyles({
   },
   yellow2: {
     color: "yellow",
-    fontWeight: "1000",
+    fontWeight: "1000"
     // fontSize: "1.2rem"
   },
   white: {
@@ -32,8 +31,8 @@ const useStyles = makeStyles({
     color: "#ff8300",
     fontWeight: "1000",
     fontSize: "1rem"
-// background: "#005DFF",
-// textShadow: "1px 1px 0px #00e6e6, 0px 0px 0px #00bdbd "
+    // background: "#005DFF",
+    // textShadow: "1px 1px 0px #00e6e6, 0px 0px 0px #00bdbd "
   },
   grayLine: {
     color: "gray",
@@ -41,13 +40,22 @@ const useStyles = makeStyles({
   }
 });
 
+const williamRoy = hubenschmidt => {
+  let page = "portfolio";
+  return "Information and " + page + " on software developer in Brooklyn.";
+};
+
+console.log(williamRoy());
+
 export default function Introduction() {
   const classes = useStyles();
   return (
     <Box component="div" className={classes.root}>
       <Box component="span" className={classes.grayLine}>
         <Typography>
-          {"// introduction--------------------------------------------------------------------------------------------------------------------------------------------"}
+          {
+            "// introduction--------------------------------------------------------------------------------------------------------------------------------------------"
+          }
         </Typography>
       </Box>
       <Box component="div">
@@ -55,93 +63,54 @@ export default function Introduction() {
           <Typography className={classes.blue} display="inline">
             const{" "}
           </Typography>
-          <Typography
-        
-            className={classes.yellow}
-            display="inline"
-          >
+          <Typography className={classes.yellow} display="inline">
             williamRoy{" "}
           </Typography>
-          <Typography
-          
-            className={classes.white}
-            display="inline"
-          >
+          <Typography className={classes.white} display="inline">
             ={" "}
           </Typography>
-          <Typography
-          
-            className={classes.yellow}
-            display="inline"
-          >
-            (hubenschmidt){" "}
+          <Typography className={classes.blue} display="inline">
+            hubenschmidt{" "}
           </Typography>
 
           <Typography className={classes.blue} display="inline">
             =>{" "}
           </Typography>
 
-          <Typography
-           
-            className={classes.white}
-            display="inline"
-          >
+          <Typography className={classes.white} display="inline">
             {"{"}{" "}
           </Typography>
           <Box component="div" marginLeft="20px">
             <Box component="div">
-              <Typography
-             
-                className={classes.white}
-                display="inline"
-              >
+              <Typography className={classes.white} display="inline">
                 page ={" "}
               </Typography>
-              <Typography
-             
-                className={classes.yellow2}
-                display="inline"
-              >
-                portfolio
+              <Typography className={classes.orange} display="inline">
+                'portfolio'
               </Typography>
-              <Typography
-                className={classes.white}
-                display="inline"
-              >
+              <Typography className={classes.white} display="inline">
                 ;
               </Typography>
             </Box>
             <Box component="div">
-              <Typography
-                className={classes.fuschia}
-                display="inline"
-              >
+              <Typography className={classes.fuschia} display="inline">
                 return{" "}
               </Typography>
-              <Typography
-                className={classes.orange}
-                display="inline"
-
-              >
+              <Typography className={classes.orange} display="inline">
                 'Information and '{" "}
               </Typography>
-              <Typography
-                className={classes.white}
-                display="inline"
-              >
-                +page+
+              <Typography className={classes.white} display="inline">
+                + page +{" "}
               </Typography>
-              <Typography
-                className={classes.orange}
-                display="inline"
-              >
-                ' on software developer in Brooklyn'{" "}
+              <Typography className={classes.orange} display="inline">
+                ' on software developer in Brooklyn.'
+              </Typography>
+              <Typography className={classes.white} display="inline">
+                ;
               </Typography>
             </Box>
           </Box>
-          <Typography className={classes.white}>
-            {"};"} 
-          </Typography>
+          <Typography className={classes.white}>{"};"}</Typography>
           <Typography className={classes.white}>
             {/* feel free to explore →↑ or contact below ←↓ */}
           </Typography>
