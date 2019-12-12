d6d9b6a097fa6bcbb4fec0c957113661cf1f0858
hubenschmidt <whubenschmidt@gmail.com>
2019-12-12 02:09:13 -0500

    build master

---
+++ build/precache-manifest.c6ffbcf7b219b39c19692523f8b9dd60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0cd06b806dc66c13b19961b4f493f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73605278b7aa134b1eb4",
+    "url": "/static/css/main.eab0b4b6.chunk.css"
+  },
+  {
+    "revision": "cb2531e8c86e95cea0d1",
+    "url": "/static/js/2.4e7e518f.chunk.js"
+  },
+  {
+    "revision": "73605278b7aa134b1eb4",
+    "url": "/static/js/main.00c80da3.chunk.js"
+  },
+  {
+    "revision": "0a21d8c0217fdb261a72",
+    "url": "/static/js/runtime~main.bd4a1de2.js"
+  }
+]);
